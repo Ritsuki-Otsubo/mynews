90087e8c4ccf8015f6dda3e807378e4ff9595c36
”ritsuki <usefull3567tuwa@gmail.com>
2020-09-23 02:51:58 +0000

    09kadai.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//ファイルでルーティングを定義します。
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+//課題３
+Route::group(['prefix' => 'XXX'], function() {
+    Route::get('news/create', 'XXX\AAAController@bbb');
+});
+
+//課題４
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
